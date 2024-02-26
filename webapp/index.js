40d17361316a3f5d360028de360a4276aa613929
HP-LAPTOP\rojas <22415328+rojasjo@users.noreply.github.com>
2024-02-26 17:50:47 +0100

    added model

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,21 @@
 sap.ui.require([
-	"sap/m/Text"
+    "sap/m/Text"
 ], function (Text) {
-	"use strict";
+    "use strict";
 
-	// Attach an anonymous function to the SAPUI5 'init' event
-	sap.ui.getCore().attachInit(function () {
-		// Create a text UI element that displays a hardcoded text string
-		new Text({text: "Hi, my name is Harry Hawk"}).placeAt("content");
-	});
+    // Attach an anonymous function to the SAPUI5 'init' event
+    sap.ui.getCore().attachInit(function () {
+        // Create a text UI element that displays a hardcoded text string
+        // Create a JSON model from an object literal
+        var oModel = new JSONModel({
+            greetingText: "Hi, my name is Harry Hawk"
+        });
+
+        // Assign the model object to the SAPUI5 core
+        sap.ui.getCore().setModel(oModel);
+
+
+        // Create a text UI element that displays a hardcoded text string
+        new Text({ text: "Hi, my name is Harry Hawk" }).placeAt("content");
+    });
 });
